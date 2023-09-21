c5ff694d93c4845d7962c7486b4b25b55a6ead68
JonAJ <jonajmail@gmail.com>
2023-09-21 12:39:36 +0300

    Fixed tests

--- lab1/tests/test_01.cpp
+++ lab1/tests/test_01.cpp
@@ -1,31 +1,41 @@
 #include <gtest/gtest.h>
 #include <limits>
+#include <cstring>
 #include "to_bin_32.h"
 
 TEST(test_01, basic_test_set)
 {
-    ASSERT_TRUE(to_bin_32(0) == "00000000000000000000000000000000");
+    char* res = to_bin_32(0);
+    ASSERT_TRUE(strcmp(res, "00000000000000000000000000000000") == 0);
+    delete[] res;
 }
 
 TEST(test_02, basic_test_set)
 {
-    ASSERT_TRUE(to_bin_32(UINT32_MAX) == "11111111111111111111111111111111");
+    char* res = to_bin_32(UINT32_MAX);
+    ASSERT_TRUE(strcmp(res, "11111111111111111111111111111111") == 0);
+    delete[] res;
 }
-
 
 TEST(test_03, basic_test_set)
 {
-    ASSERT_TRUE(to_bin_32(4627884) == "00000000010001101001110110101100");
+    char* res = to_bin_32(4627884);
+    ASSERT_TRUE(strcmp(res, "00000000010001101001110110101100") == 0);
+    delete[] res;
 }
 
 TEST(test_04, basic_test_set)
 {
-    ASSERT_TRUE(to_bin_32(79828742) == "00000100110000100001011100000110");
+    char* res = to_bin_32(79828742);
+    ASSERT_TRUE(strcmp(res, "00000100110000100001011100000110") == 0);
+    delete[] res;
 }
 
 TEST(test_05, basic_test_set)
 {
-    ASSERT_TRUE(to_bin_32(10000) == "00000000000000000010011100010000");
+    char* res = to_bin_32(10000);
+    ASSERT_TRUE(strcmp(res, "00000000000000000010011100010000") == 0);
+    delete[] res;
 }
 
 
