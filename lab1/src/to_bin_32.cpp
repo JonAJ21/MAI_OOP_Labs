04ac713cf3dd1e91510efb26419ddd751ce43143
JonAJ <jonajmail@gmail.com>
2023-09-22 19:48:24 +0300

    Added std::bad_alloc exception

--- lab1/src/to_bin_32.cpp
+++ lab1/src/to_bin_32.cpp
@@ -2,9 +2,12 @@
 
 char* to_bin_32(uint32_t num) {
     const int SZ = 32;
-    char* str = new char[SZ + 1];
-    if (str == nullptr) {
-        return nullptr;
+    char* str;
+    try {
+        str = new char[SZ + 1];
+    }
+    catch (const std::bad_alloc& exception) {
+        std::cerr << "Error: " << exception.what() << std::endl;
     }
     for (int i = 0; i < SZ; ++i) {
         str[i] = ((num >> (SZ - i - 1)) & 1) + '0';
