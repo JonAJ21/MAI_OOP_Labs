9a28f5caca513a0ea0a88068abd9a484f262d36d
JonAJ <jonajmail@gmail.com>
2023-10-18 21:38:03 +0300

    Started making task

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,11 +3,18 @@
 #include "square.h"
 #include "rectangle.h"
 #include "trapezoid.h"
-
+#include <typeinfo>
 int main() {
     
-    Trapezoid a;
-    Trapezoid b {{0, 0}, {2, 4}, {7, 4}, {10, 1}};
+    // Figure* figures = new Square({0, 0}, {0, 1}, {1, 1}, {1, 0});
+    // std::cout << figures->area() << std::endl;
+    Figure** figures = new Figure* [2];
+    figures[0] = new Square({0, 0}, {0, 1}, {1, 1}, {1, 0});
+    figures[1] = new Rectangle({0, 0}, {0, 2}, {3, 2}, {3, 0});
+    // for (int i = 0; i < 2; ++i) {
+    //     // std::cout << *figures[0] << std::endl;
+    // }
+    Square sq = *figures[0];
 
 
 
---
+++ lab3/src/task.cpp
@@ -0,0 +1,11 @@
+#include "task.h"
+
+Task::Task() : _figures(nullptr), _sz(0) {
+}
+
+// Task::Task(std::initializer_list<Figure> const & init_list) : _sz(init_list.size()) {
+//     _figures = new Figure [_sz];
+//     for (size_t i = 0; i < _sz; ++i) {
+        
+//     }
+// }
