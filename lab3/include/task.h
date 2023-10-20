a16d9559dd44cf5868c923ad6d40763c5e2c3b44
JonAJ <jonajmail@gmail.com>
2023-10-20 22:46:07 +0300

    some changes

--- lab3/include/task.h
+++ lab3/include/task.h
@@ -11,25 +11,8 @@ private:
     Figure* _figures;
     size_t _sz;
 public:
-    Task();
-    Task(std::initializer_list<Figure> const & init_list);
-    Task(Task const & t);
-    Task(Task&& t);
-    Task& operator=(Task const & t);
-    Task& operator=(Task&& t);
-    ~Task() noexcept;
-
-    bool push_back(Figure const & fig);
-    bool insert(Figure const & fig, size_t idx);
-    bool pop(size_t idx);
-    void print_areas();
-    void print_centres();
-    double total_area();
-
-    void enter_figures();
-
-    // TODO
-    // Операции сравнения
+    friend std::istream& operator>>(std::istream& is, Square & sq);
+    friend std::ostream& operator<<(std::ostream& os, Square const & sq); 
 };
 
 #endif // TASK_H
