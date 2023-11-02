8897011d3486a9e030661c14c84c9e4f5caec8bb
JonAJ <jonajmail@gmail.com>
2023-11-02 10:45:10 +0300

    backup

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include "point2d.h"
 #include "figure.h"
 #include "square.h"
@@ -7,13 +8,9 @@
 
 int main() {
     
-    Point2D<int> p_1 {1, 2};
-    // ASSERT_TRUE(p_1.x == 1 && p_1.y == 2);
+    std::shared_ptr<std::shared_ptr<Figure<double>>> figures;
     
-    Point2D<double> p_2 {1.3, 4.5};
-    // ASSERT_TRUE(p_2.x == 1.3 && p_2.y == 4.5);
-
-    std::cout << are_equal(p_2.x, p_2.x, 1e-6);
+    
 
     return 0;
 }  
