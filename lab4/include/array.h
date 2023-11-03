4831366440a00cf1247d9a90c2f95fb08573d914
JonAJ <jonajmail@gmail.com>
2023-11-03 21:28:31 +0300

    Finished array.h

--- lab4/include/array.h
+++ lab4/include/array.h
@@ -14,6 +14,7 @@
 template<typename T>
 class Array final {
 private:
+    size_t _capacity;
     size_t _sz;
     std::shared_ptr<std::shared_ptr<Figure<T>>[]> _figures;
 public:
@@ -32,29 +33,29 @@ public:
     bool is_empty() const;
     size_t size() const;
 
-
     void print() const;
 };
 
 template<typename T>
-Array<T>::Array() : _sz(0), _figures(nullptr){
+Array<T>::Array() : _sz(0), _capacity(0), _figures(nullptr) {
     std::cout << "Default constructor" << std::endl;
 }
 
 template<typename T>
-Array<T>::Array(std::initializer_list<std::shared_ptr<Figure<T>>> const & init_list) : _sz(init_list.size()) {
+Array<T>::Array(std::initializer_list<std::shared_ptr<Figure<T>>> const & init_list) : _sz(init_list.size()), _capacity(init_list.size() * 2) {
     std::cout << "Init list constructor" << std::endl;
-    _figures = std::make_shared< std::shared_ptr<Figure<T>>[] >(_sz);
+    _figures = std::make_shared< std::shared_ptr<Figure<T>>[] >(_capacity);
     size_t i = 0;
     for (auto figure_ptr : init_list) {
         _figures[i] = figure_ptr;
         ++i;
     }
+    std::cout << "init" << std::endl;
 }
 
 template<typename T>
-Array<T>::Array(Array const & array) : _sz(array._sz) {
-    _figures = std::make_shared< std::shared_ptr<Figure<T>>[] >(_sz);
+Array<T>::Array(Array const & array) : _sz(array._sz), _capacity(array._sz * 2) {
+    _figures = std::make_shared< std::shared_ptr<Figure<T>>[] >(_capacity);
     for (size_t i = 0; i < _sz; ++i) {
         if (typeid(*array._figures[i]) == typeid(Square<T>)) {
             std::shared_ptr<Square<T>> square_ptr{std::dynamic_pointer_cast<Square<T>>(array._figures[i])}; 
@@ -75,9 +76,10 @@ Array<T>::Array(Array const & array) : _sz(array._sz) {
 }
 
 template<typename T>
-Array<T>::Array(Array<T>&& array) : _sz(array._sz), _figures(array._figures) {
+Array<T>::Array(Array<T>&& array) : _sz(array._sz), _capacity(array._capacity), _figures(array._figures) {
     array._figures = nullptr;
     array._sz = 0;
+    array._capacity = 0;
 }
 
 template<typename T>
@@ -117,6 +119,70 @@ Array<T>& Array<T>::operator=(Array&& array) {
 template<typename T>
 Array<T>::~Array() {
     std::cout << "Destructor" << std::endl; // Можно поставить  = default
+}
+
+template<typename T>
+std::shared_ptr<Figure<T>>& Array<T>::operator[](size_t idx) {
+    if (!(0 <= idx && idx < _sz)) {
+        throw std::invalid_argument("Index out of range");
+    } 
+    return _figures[idx];
+}
+
+template<typename T>
+Array<T>& Array<T>::push_back(std::shared_ptr<Figure<T>> const & figure_ptr) {
+    if (_sz == _capacity) {
+        _capacity *= 2;
+        // std::cout << "Here" << std::endl;
+        std::shared_ptr<std::shared_ptr<Figure<T>>[]> temp;
+        temp = std::make_shared< std::shared_ptr<Figure<T>>[] >(_capacity);
+        for (size_t i = 0; i < _sz; ++i) {
+            temp[i] = _figures[i];
+        }
+        _figures = temp;
+    }
+    _figures[_sz++] = figure_ptr;
+    return *this;
+}
+
+template<typename T>
+Array<T>& Array<T>::insert(std::shared_ptr<Figure<T>> const & figure_ptr, size_t idx) {
+    if (!(0 <= idx && idx <= _sz)) {
+        throw std::invalid_argument("Index out of range");
+    } 
+    if (_sz == _capacity) {
+        _capacity *= 2;
+        std::shared_ptr<std::shared_ptr<Figure<T>>[]> temp;
+        temp = std::make_shared< std::shared_ptr<Figure<T>>[] >(_capacity);
+        for (size_t i = 0; i < _sz; ++i) {
+            temp[i] = _figures[i];
+        }
+        _figures = temp;
+    }
+    for (size_t i = _sz; i > idx; --i) {
+        _figures[i] = _figures[i - 1];
+    }
+    _figures[idx] = figure_ptr;
+    ++_sz;
+    return *this;
+}
+
+template<typename T>
+Array<T>& Array<T>::remove(size_t idx) {
+    if (!(0 <= idx && idx < _sz)) {
+        throw std::invalid_argument("Index out of range");
+    }
+    --_sz;
+    for (size_t i = idx; i < _sz; ++i) {
+        _figures[i] = _figures[i + 1];
+    }
+    _figures[_sz] = nullptr;
+    return *this;
+}
+
+template<typename T>
+bool Array<T>::is_empty() const {
+    return _sz == 0;
 }
 
 template<typename T>
--- lab4/main.cpp
+++ lab4/main.cpp
@@ -10,30 +10,21 @@
 #include <typeinfo>
 
 int main() {
-    // int sz = 10;
-    // std::shared_ptr<std::shared_ptr<Figure<double>>[]> figures = std::make_shared< std::shared_ptr<Figure<double>>[] >(sz);
-    // figures[0] = std::make_shared<Square<double>>(Square<double>({0,0}, {0, 1}, {1, 1}, {1, 0}));
-    // figures[1] = std::make_shared<Rectangle<double>>(Rectangle<double>({0,0}, {0, 2}, {4, 2}, {4, 0}));
-    // figures[2] = std::make_shared<Rectangle<double>>(Rectangle<double>({0,0}, {0, 2}, {4, 2}, {4, 0}));
-    // std::cout << typeid(*figures[0]).name() << std::endl;
-    // std::cout << typeid(*figures[1]).name() << std::endl;
-    // std::cout << typeid(*figures[2]).name() << std::endl;
-    // std::cout << (typeid(*figures[2]) == typeid(Rectangle<double>)) << std::endl;
-
-
-    // std::cout << "Area 0: " << (double) *figures[0] << std::endl;
-    // std::cout << "Area 1: " << figures[1]->area() << std::endl;
-
-    auto fig_1 = std::make_shared<Square<double>>(Square<double>({0,0}, {0, 1}, {1, 1}, {1, 0}));
-    auto fig_2 = std::make_shared<Rectangle<double>>(Rectangle<double>({0,0}, {0, 2}, {4, 2}, {4, 0}));
-
-    Array<double> array_1 {fig_1, fig_2};
-    array_1.print();
-
-    Array<double> array_2;
-    array_2 = std::move(array_1);
+    // auto fig_1 = std::make_shared<Square<double>>(Square<double>({0,0}, {0, 1}, {1, 1}, {1, 0}));
+    // auto fig_2 = std::make_shared<Rectangle<double>>(Rectangle<double>({0,0}, {0, 2}, {4, 2}, {4, 0}));
+    // auto fig_3 = std::make_shared<Rectangle<double>>(Rectangle<double>({0,0}, {0, 2}, {4, 2}, {4, 0}));
+    // auto fig_4 = std::make_shared<Square<double>>(Square<double>({0,0}, {0, 1}, {1, 1}, {1, 0}));
+    // auto fig_5 = std::make_shared<Square<double>>(Square<double>({0,0}, {0, 1}, {1, 1}, {1, 0}));
     
-    array_2.print();
+    std::cout << "What type of points do you want to store in the array?" << std::endl;
+    std::cout << "1. int" << std::endl;
+    std::cout << "2. double" << std::endl;
+    int type_num = 0;
+    std::cin >> type_num;
+    
+    
+
+
 
     return 0;
 }  
