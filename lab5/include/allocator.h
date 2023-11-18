3d777f671ac8984d6f75125994120f13497e4a97
JonAJ <jonajmail@gmail.com>
2023-11-18 13:15:50 +0300

    Some changes in allocator.h

--- lab5/include/allocator.h
+++ lab5/include/allocator.h
@@ -5,20 +5,13 @@
 #include <deque>
 
 namespace mai {
-    
+
     template<typename T>
     class Allocator {
-    private:
-        std::deque<T*> _used_blocks;
-        std::deque<T*> _free_blocks;
+    private:    
+        std::deque<std::pair<size_t, T*>> _used_blocks;
+        std::deque<std::pair<size_t, T*>> _free_blocks;
     public:
-         // Так и не понял, зачем это пишут в примере.
-        using value_type = T;
-        using ptr = T*;
-        using const_ptr = T const *;
-        using size_type = size_t;
-
-
         Allocator() = default;
         // Allocator(Allocator const & other);
         // Allocator(Allocator&& other);
@@ -32,43 +25,111 @@ namespace mai {
         T* allocate(size_t n);
         void deallocate(T* ptr, size_t size);
 
-        // template <typename T, typename U>
-        // constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
+        template <typename T, typename U>
+        constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
 
-        // template <typename T, typename U>
-        // constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
+        template <typename T, typename U>
+        constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
     };
-
-    
-
-    template<typename T>
-    Allocator<T>::~Allocator() {
-        size_t size = _used_blocks.size();
-        for (size_t i = 0; i < size; ++i) {
-            T* ptr = _used_blocks.back();
-            _used_blocks.pop_back();
-            delete ptr;
-        }
-    }
 
     template<typename T>
     T* Allocator<T>::allocate(size_t n) {
-        if (_free_blocks.size() > 0 && n == 1) {
-            T* ptr = _free_blocks.back();
-            _free_blocks.pop_back();
-            return ptr;
+        for (size_t i = 0; i < _free_blocks.size(); ++i) {
+            if (n <= _free_blocks[i].first) {
+                T* ptr = _free_blocks[i];
+                _used_blocks.push_back(ptr);
+                _free_blocks.erase(_free_blocks.begin() + i);
+                return ptr;
+            }
         }
         T* ptr = reinterpret_cast<T*>(::operator new(n * sizeof(T)));
-        _used_blocks.push_back(ptr);
+        _used_blocks.push_back({n, ptr});
         return ptr;
     }
 
-    template<typename T> 
-    void Allocator<T>::deallocate(T* ptr, size_t size) {
-        for (size_t i = 0; i < size; ++i) {
-            _free_blocks.push_back(ptr + i * sizeof(T));
+    template<typename T>
+    void Allocator::deallocate(T* ptr, size_t size) {
+        for (size_t i = 0; i < _used_blocks.size(); ++i) {
+            if (_used_blocks[i].first == size && _used_blocks[i].second == ptr) {
+                _free_blocks.push_back(_used_blocks[i]);
+                _used_blocks.erase(_used_blocks.begin() + i);
+                return;
+            }
         }
+        throw "Eception"; // Надо изменить
     }
+
+
+
+
+    
+    // template<typename T>
+    // class Allocator {
+    // private:
+    //     std::deque<T*> _used_blocks;
+    //     std::deque<T*> _free_blocks;
+    // public:
+    //     // Так и не понял, зачем это пишут в примере.
+    //     // using value_type = T;
+    //     // using ptr = T*;
+    //     // using const_ptr = T const *;
+    //     // using size_type = size_t;
+
+    //     Allocator() = default;
+    //     Allocator(Allocator const & other);
+    //     // Allocator(Allocator&& other);
+    //     // Allocator& operator=(Allocator<T> const & other);
+    //     // Allocator& operator=(Allocator<T>&& other);
+    //     ~Allocator();
+
+    //     template<typename U>
+    //     struct rebind {using other = Allocator<U>;};
+
+    //     T* allocate(size_t n);
+    //     void deallocate(T* ptr, size_t size);
+
+    //     // template <typename T, typename U>
+    //     // constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
+
+    //     // template <typename T, typename U>
+    //     // constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
+    // };
+
+    // template<typename T>
+    // Allocator<T>::Allocator(Allocator const & other) {
+        
+    // }
+
+    
+    // template<typename T>
+    // Allocator<T>::~Allocator() {
+    //     size_t size = _used_blocks.size();
+    //     for (size_t i = 0; i < size; ++i) {
+    //         T* ptr = _used_blocks.back();
+    //         _used_blocks.pop_back();
+    //         delete ptr;
+    //     }
+    // }
+
+    // template<typename T>
+    // T* Allocator<T>::allocate(size_t n) {
+    //     if (_free_blocks.size() > 0 && n == 1) {
+    //         T* ptr = _free_blocks.back();
+    //         _free_blocks.pop_back();
+    //         return ptr;
+    //     }
+    //     T* ptr = reinterpret_cast<T*>(::operator new(n * sizeof(T)));
+    //     _used_blocks.push_back(ptr);
+    //     return ptr;
+
+    // }
+
+    // template<typename T> 
+    // void Allocator<T>::deallocate(T* ptr, size_t size) {
+    //     for (size_t i = 0; i < size; ++i) {
+    //         _free_blocks.push_back(ptr + i * sizeof(T));
+    //     }
+    // }
 
 
 }
