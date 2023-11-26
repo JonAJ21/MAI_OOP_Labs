d0e5e6d388694c92abd125c72be11cd3c6295a6e
JonAJ <jonajmail@gmail.com>
2023-11-26 19:23:34 +0300

    lab5

--- lab5/include/allocator.h
+++ lab5/include/allocator.h
@@ -12,6 +12,11 @@ namespace mai {
         std::deque<std::pair<size_t, T*>> _used_blocks;
         std::deque<std::pair<size_t, T*>> _free_blocks;
     public:
+        using value_type = T;
+        using ptr = T*;
+        using const_ptr = T const *;
+        using size_type = size_t;
+
         Allocator() = default;
         // Allocator(Allocator const & other);
         // Allocator(Allocator&& other);
@@ -25,19 +30,29 @@ namespace mai {
         T* allocate(size_t n);
         void deallocate(T* ptr, size_t size);
 
-        template <typename T, typename U>
-        constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
+        // template <typename T, typename U>
+        // constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
 
-        template <typename T, typename U>
-        constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
+        // template <typename T, typename U>
+        // constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
     };
 
     template<typename T>
-    T* Allocator<T>::allocate(size_t n) {
+    Allocator<T>::~Allocator() {
+        std::cout << -1 << std::endl;
+        size_t free_blocks_size = _free_blocks.size();
+        for (size_t i = 0; i < free_blocks_size; ++i) {
+            T* ptr = _free_blocks[i].second;
+            delete ptr;
+        }
+    }
+
+    template<typename T>
+    T* Allocator<T>::allocate(size_t n) { // Скорость тоже оставляет желать лучшего
         for (size_t i = 0; i < _free_blocks.size(); ++i) {
             if (n <= _free_blocks[i].first) {
-                T* ptr = _free_blocks[i];
-                _used_blocks.push_back(ptr);
+                T* ptr = _free_blocks[i].second;
+                _used_blocks.push_back(_free_blocks[i]);
                 _free_blocks.erase(_free_blocks.begin() + i);
                 return ptr;
             }
@@ -48,90 +63,18 @@ namespace mai {
     }
 
     template<typename T>
-    void Allocator::deallocate(T* ptr, size_t size) {
+    void Allocator<T>::deallocate(T* ptr, size_t size) { // ОЧЕНЬ медленно работает
         for (size_t i = 0; i < _used_blocks.size(); ++i) {
             if (_used_blocks[i].first == size && _used_blocks[i].second == ptr) {
                 _free_blocks.push_back(_used_blocks[i]);
                 _used_blocks.erase(_used_blocks.begin() + i);
                 return;
             }
+            // Вопрос: что делать в остальных случаях?
+            //         как ускорить?
         }
-        throw "Eception"; // Надо изменить
+        throw std::logic_error("Error: deallocate");
     }
-
-
-
-
-    
-    // template<typename T>
-    // class Allocator {
-    // private:
-    //     std::deque<T*> _used_blocks;
-    //     std::deque<T*> _free_blocks;
-    // public:
-    //     // Так и не понял, зачем это пишут в примере.
-    //     // using value_type = T;
-    //     // using ptr = T*;
-    //     // using const_ptr = T const *;
-    //     // using size_type = size_t;
-
-    //     Allocator() = default;
-    //     Allocator(Allocator const & other);
-    //     // Allocator(Allocator&& other);
-    //     // Allocator& operator=(Allocator<T> const & other);
-    //     // Allocator& operator=(Allocator<T>&& other);
-    //     ~Allocator();
-
-    //     template<typename U>
-    //     struct rebind {using other = Allocator<U>;};
-
-    //     T* allocate(size_t n);
-    //     void deallocate(T* ptr, size_t size);
-
-    //     // template <typename T, typename U>
-    //     // constexpr bool operator==(Allocator<T> const & lhs, Allocator<U> const & rhs);
-
-    //     // template <typename T, typename U>
-    //     // constexpr bool operator!=(Allocator<T> const & lhs, Allocator<U> const & rhs);
-    // };
-
-    // template<typename T>
-    // Allocator<T>::Allocator(Allocator const & other) {
-        
-    // }
-
-    
-    // template<typename T>
-    // Allocator<T>::~Allocator() {
-    //     size_t size = _used_blocks.size();
-    //     for (size_t i = 0; i < size; ++i) {
-    //         T* ptr = _used_blocks.back();
-    //         _used_blocks.pop_back();
-    //         delete ptr;
-    //     }
-    // }
-
-    // template<typename T>
-    // T* Allocator<T>::allocate(size_t n) {
-    //     if (_free_blocks.size() > 0 && n == 1) {
-    //         T* ptr = _free_blocks.back();
-    //         _free_blocks.pop_back();
-    //         return ptr;
-    //     }
-    //     T* ptr = reinterpret_cast<T*>(::operator new(n * sizeof(T)));
-    //     _used_blocks.push_back(ptr);
-    //     return ptr;
-
-    // }
-
-    // template<typename T> 
-    // void Allocator<T>::deallocate(T* ptr, size_t size) {
-    //     for (size_t i = 0; i < size; ++i) {
-    //         _free_blocks.push_back(ptr + i * sizeof(T));
-    //     }
-    // }
-
-
 }
 
 #endif // ALLOCATOR_H
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -4,72 +4,101 @@
 #include "queue.h"
 #include "allocator.h"
 
+#include <deque>
+
 int main() {
-    // {
-    //     auto begin = std::chrono::high_resolution_clock::now();
-    //     using Allocator = mai::Allocator<std::pair<const int, int>>;
-    //     std::map<int, int, std::less<int>, Allocator> m;
-    //     // std::map<int, int> m;
-    //     int64_t counter = 1;
-    //     m.insert(std::pair<int, int>(0, 0));
-    //     for (int64_t i = 1; i < 5000000; ++i) {
-    //         counter *= i;
-    //         m.insert(std::pair<int, int>(i, counter));
-    //     }
-    //     auto end = std::chrono::high_resolution_clock::now();
-    //     std::cout << "test1: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+
+    // std::deque<int> d {1, 2, 3, 4, 5};
+    // for (auto el : d) {
+    //     std::cout << el << ' ';
     // }
-    
-    // {
-    //     auto begin = std::chrono::high_resolution_clock::now();
-    //     using Allocator = std::allocator<std::pair<const int, int>>;
-    //     std::map<int, int, std::less<int>, Allocator> m;
-    //     // std::map<int, int> m;
-    //     int64_t counter = 1;
-    //     m.insert(std::pair<int, int>(0, 0));
-    //     for (int64_t i = 1; i < 5000000; ++i) {
-    //         counter *= i;
-    //         m.insert(std::pair<int, int>(i, counter));
-    //     }
-    //     auto end = std::chrono::high_resolution_clock::now();
-    //     std::cout << "test2: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+    // std::cout << std::endl;
+
+    // d.erase(d.begin() + 3);
+    // for (auto el : d) {
+    //     std::cout << el << ' ';
     // }
+
+    // std::map<int, int, std::less<int>, mai::Allocator<std::pair<const int, int>>> m;
+
+    // int64_t counter = 1;
+    // m.insert(std::pair<int, int>(0, 0));
+    // for (int64_t i = 1; i < 10; ++i) {
+    //     counter *= i;
+    //     m.insert(std::pair<int, int>(i, counter));
+    // }
+
+    // for (auto e: m) {
+    //     std::cout << e.first << ' ' << e.second << '\n';
+    // }
+
+
+
+    {
+        auto begin = std::chrono::high_resolution_clock::now();
+        using Allocator = mai::Allocator<std::pair<const int, int>>;
+        std::map<int, int, std::less<int>, Allocator> m;
+        // std::map<int, int> m;
+        int64_t counter = 1;
+        m.insert(std::pair<int, int>(0, 0));
+        for (int64_t i = 1; i < 1000; ++i) {
+            counter *= i;
+            m.insert(std::pair<int, int>(i, counter));
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "test1: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+        std::cout << "KDLS";
+    }
+    {
+        auto begin = std::chrono::high_resolution_clock::now();
+        using Allocator = std::allocator<std::pair<const int, int>>;
+        std::map<int, int, std::less<int>, Allocator> m;
+        // std::map<int, int> m;
+        int64_t counter = 1;
+        m.insert(std::pair<int, int>(0, 0));
+        for (int64_t i = 1; i < 1000; ++i) {
+            counter *= i;
+            m.insert(std::pair<int, int>(i, counter));
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "test2: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+    }
     
     // for (auto e: m) {
     //     std::cout << e.first << ' ' << e.second << '\n';
     // }
 
-    mai::Queue<int, mai::Allocator<int>> q{10, 2, 3, 4, 5};
+    // mai::Queue<int, mai::Allocator<int>> q{10, 2, 3, 4, 5};
     
-    mai::Queue<int, mai::Allocator<int>> q2;
-    q2 = q;
+    // mai::Queue<int, mai::Allocator<int>> q2;
+    // q2 = q;
 
-    // std::cout << q << std::endl;
-    q.print(std::cout);
-    std::cout << "capacity: " << q.capacity() << std::endl;
-    std::cout << "size: " << q.size() << std::endl;
-    std::cout << std::boolalpha << "empty: " << q.empty() << std::endl;
+    // // std::cout << q << std::endl;
+    // q.print(std::cout);
+    // std::cout << "capacity: " << q.capacity() << std::endl;
+    // std::cout << "size: " << q.size() << std::endl;
+    // std::cout << std::boolalpha << "empty: " << q.empty() << std::endl;
 
-    q.push(125);
-    q.push(125);
-    q.push(125);
-    q.push(125);
-    q.push(125);
-    q.pop();
-    q.pop();
-    q.pop();
-    q.pop();
-    q.pop();
-    q.pop();
-    q.push(125);
-    q.push(125);
-    q.push(125);
-    // std::cout << q << std::endl;
-    // q.reserve(3);
-    q.print(std::cout);
-    std::cout << "capacity: " << q.capacity() << std::endl;
-    std::cout << "size: " << q.size() << std::endl;
-    std::cout << std::boolalpha << "empty: " << q.empty() << std::endl;
+    // q.push(125);
+    // q.push(125);
+    // q.push(125);
+    // q.push(125);
+    // q.push(125);
+    // q.pop();
+    // q.pop();
+    // q.pop();
+    // q.pop();
+    // q.pop();
+    // q.pop();
+    // q.push(125);
+    // q.push(125);
+    // q.push(125);
+    // // std::cout << q << std::endl;
+    // // q.reserve(3);
+    // q.print(std::cout);
+    // std::cout << "capacity: " << q.capacity() << std::endl;
+    // std::cout << "size: " << q.size() << std::endl;
+    // std::cout << std::boolalpha << "empty: " << q.empty() << std::endl;
 
 
 
